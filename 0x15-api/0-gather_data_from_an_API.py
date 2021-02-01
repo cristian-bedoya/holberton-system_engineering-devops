de6d7fa7d5320e1c9827fb07980822ff39f4d124
cristian-bedoya <crisbed321@gmail.com>
2021-02-01 23:33:36 +0000

    organize importation

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Using this REST API, for a given employee ID, returns
     information about his/her TODO list progress"""
-from sys import argv
 import requests
+from sys import argv
 
 if __name__ == "__main__":
 
